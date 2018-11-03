db5fa7427cb27f44be59c34b1335abd82eceff9d
Solve Dahlin <solvedsaeter@gmail.com>
2018-11-03 12:59:58 +0100

    Added /speedtest/ping/

---
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpeedTestApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SpeedTestController : ControllerBase
+    {
+        // GET speedtest/ping
+        [Route("ping")]
+        [HttpGet]
+        public ActionResult<string> Ping()
+        {
+            return Ok("PONG");
+        }
+    }
+}
