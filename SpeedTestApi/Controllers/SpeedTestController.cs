9747dac1127538d28fb41c71bb01846b0ac83231
Solve Dahlin <solvedsaeter@gmail.com>
2018-11-03 14:49:56 +0100

    Changes in SpeedTestApi

--- SpeedTestApi/Controllers/SpeedTestController.cs
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SpeedTestApi.Models;
 
 namespace SpeedTestApi.Controllers
 {
@@ -16,6 +17,15 @@ namespace SpeedTestApi.Controllers
         public ActionResult<string> Ping()
         {
             return Ok("PONG");
+        }
+
+        // POST speedtest/
+        [HttpPost]
+        public ActionResult<string> UploadSpeedTest([FromBody] TestResult speedTest)
+        {
+            var speedTestData = $"Got a TestResult from { speedTest.User } with download { speedTest.Data.Speeds.Download } Mbps.";
+
+            return Ok(speedTestData);
         }
     }
 }
